6404db02624e8fc8c9222628f85f3a518f146a9b
fauzan2720 <fauzan.abdillah2705@gmail.com>
2021-04-02 19:25:43 +0700

    Update menampilkan_kelipatan_dan_perkalian_dua.java

---
+++ Minggu4/src/Task/menampilkan_kelipatan_dan_perkalian_dua.java
@@ -0,0 +1,36 @@
+package Task;
+
+/**
+ *
+ * @author Fauzan
+ */
+public class menampilkan_kelipatan_dan_perkalian_dua {
+    public static void main(String[] args) {
+        // menampilkan bilangan kelipatan 2
+        System.out.println("         For_Loop           ");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        
+        for (int i = 0; i <= 100;) { // batas perulangan jika j bernilai 100 kebawah
+            System.out.print(" " +i +" ");
+            i = i + 2; // setiap nilai j nya ditambah 2
+            if(i%7 == 0) {
+                System.out.println("");
+            }
+        }
+        System.out.println("");
+        System.out.println(""); // end
+        
+        // menampilkan bilangan perkalian 2
+        System.out.println("         Do_While           ");
+        System.out.println("Bilangan perkalian 2 (1-100)");
+        System.out.println("============================");
+        
+        int j = 2;
+        do {
+            System.out.print(" " +j +" ");
+            j = j * 2; // setiap nilai i nya dikalikan 2
+        } while(j <= 100); // batas perulangan jika i bernilai 100 kebawah
+        System.out.println(""); // end
+    }
+}
